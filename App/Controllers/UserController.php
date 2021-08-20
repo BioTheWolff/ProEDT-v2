b1f0dd206c9de242b97710c5531a1a4d836ebffe
Fabien Zoccola <biothewolff@gmx.fr>
2021-08-20 21:08:55 +0200

    user is now automatically redirected upon reaching login if already connected

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -3,19 +3,34 @@
 namespace App\Controllers;
 
 use App\Database\Interactions\UserInteraction;
+use App\Services\Session\SessionInterface;
 use Laminas\Diactoros\Response\RedirectResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class UserController extends AbstractController
 {
+    private function check_not_connected()
+    {
+        $session = $this->container->get(SessionInterface::class);
+        if (!is_null($session->get('__user')))
+        {
+            // the user is connected
+            return new RedirectResponse('/');
+        }
+        else return null;
+    }
+
     public function login_GET(): ResponseInterface
     {
+        if (($rr = $this->check_not_connected()) != null) return $rr;
         return $this->html_render("user/login");
     }
 
     public function login_POST(ServerRequestInterface $request): ResponseInterface
     {
+        if (($rr = $this->check_not_connected()) != null) return $rr;
+
         $body = $request->getParsedBody();
         $interaction = $this->container->get(UserInteraction::class);
 
