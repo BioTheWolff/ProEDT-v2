764908292a0dbd620c944f0ad154af0ee723bfdc
Rtinox <nathanrouche30@gmail.com>
2021-09-02 19:49:21 +0000

    added about page and script for settings page

---
+++ templates/about.php
@@ -0,0 +1,25 @@
+<?php
+$this->layout('_template');
+?>
+
+
+<div class="card">
+  <div class="card-header">
+    <div class="card-title h5">A propos</div>
+  </div>
+  <div class="card-body">
+    Développé par Nathan (<a href="https://github.com/Rtinox" target="_blank">_Rtinox</a>) et Fabien (<a href="https://github.com/fabien-zoccola" target="_blank">BioTheWolff</a>).<br>
+    Le projet open-source est disponible sur <a href="https://github.com/fabien-zoccola/ProEDT-v2">Github</a>.<br>
+    En cas de soucis, vous pouvez contacter Nathan via Discord: _Rtinox#4442.
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-header">
+    <div class="card-title h5">Informations légales</div>
+  </div>
+  <div class="card-body">
+    Ce site/app c'est en aucun cas affilié à l'IUT de Montpellier ou encore à l'université de Montpellier.<br>
+    Hébergement <span style="font-weight: bold;">PulseHeberg - 55, Avenue des Champs Pierreux - 92012 Nanterre Cedex.</span>
+  </div>
+</div>
--- templates/settings.php
+++ templates/settings.php
@@ -9,7 +9,7 @@ $this->layout('_template');
       Une fois sélectionné, cliquez sur "enregistrer".</div>
   </div>
   <div class="card-body">
-    <select class="form-select">
+    <select class="form-select" id="groupe-select">
       <option disabled selected value>Groupe de TD</option>
       <option>s1</option>
       <option>s2</option>
@@ -23,6 +23,42 @@ $this->layout('_template');
       <option>q4</option>
       <option>q5</option>
     </select>
-    <button class="btn">Enregistrer</button>
+    <button class="btn" onclick="saveGroup()">Enregistrer</button>
   </div>
 </div>
+
+<script>
+  function saveGroup() {
+    const groupe_seleect = document.getElementById("groupe-select");
+    if (groupe_seleect.value === "") return;
+    else {
+      setCookie("groupe", groupe_seleect.value, 365);
+      document.location.href = "/";
+    }
+  }
+
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  }
+</script>
