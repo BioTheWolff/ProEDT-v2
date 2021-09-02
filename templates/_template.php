89e3baca6ced0fa3871f6ac5269541f6728c05d0
Rtinox <36574958+Rtinox@users.noreply.github.com>
2021-09-02 07:25:25 +0000

    cdn icons in cdn/

--- templates/_template.php
+++ templates/_template.php
@@ -18,7 +18,7 @@ $this->flashes = $neon->get();
         <link rel="stylesheet" href="/assets/css/index.css" />
 
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet" />
-        <link href="/cdn/materialdesignicons.min.css" rel="stylesheet" />
+        <link href="/cdn/css/materialdesignicons.min.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
 
