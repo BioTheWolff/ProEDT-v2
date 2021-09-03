d5d973a69c8eab9a547a2df082f667a835e9d4d7
Rtinox <nathanrouche30@gmail.com>
2021-09-03 14:38:52 +0000

    page settings avec ecole

--- templates/settings.php
+++ templates/settings.php
@@ -2,44 +2,79 @@
 $this->layout('_template');
 ?>
 
-<div class="card">
+<div class="card p-centered" style="max-width: 400px;">
   <div class="card-header">
     <div class="card-title h5">Votre groupe</div>
     <div class="card-subtitle">Veuillez choisir votre groupe de TD pour accéder à l'emploi du temps, au devoirs et informations.<br>
       Une fois sélectionné, cliquez sur "enregistrer".</div>
   </div>
   <div class="card-body">
-    <select class="form-select" id="groupe-select">
-      <option disabled selected value>Groupe de TD</option>
-      <option>s1</option>
-      <option>s2</option>
-      <option>s3</option>
-      <option>s4</option>
-      <option>s5</option>
-      <option>s6</option>
-      <option>q1</option>
-      <option>q2</option>
-      <option>q3</option>
-      <option>q4</option>
-      <option>q5</option>
+    <select class="form-select" id="ecole-select" onchange="on_ecole_selected()">
+      <option disabled selected value>Ecole et filière</option>
+    </select>
+
+    <select class="form-select" id="groupe-select" disabled>
+      <option disabled selected value>Groupe</option>
     </select>
     <button class="btn" onclick="saveGroup()">Enregistrer</button>
   </div>
 </div>
 
 <script>
-  const groupe_seleect = document.getElementById("groupe-select");
+  const ecole_select = document.getElementById("ecole-select");
+  const groupe_select = document.getElementById("groupe-select");
+  const promos = [{
+    text: "IUT Informatique",
+    api_code: "iut",
+    groupes: [
+      "s1",
+      "s2",
+      "s3",
+      "s4",
+      "s5",
+      "s6",
+      "q1",
+      "q2",
+      "q3",
+      "q4",
+      "q5",
+      "q6"
+    ]
+  }];
 
   window.onload = function(e) {
-    let groupe_cookie = getCookie("groupe");
-    if(groupe_cookie != null) groupe_seleect.value = groupe_cookie;
+    for (let ecole in promos) {
+      let option = document.createElement("option");
+      option.innerText = promos[ecole].text;
+      ecole_select.append(option);
+    }
+
+    let ecole_cookie = getCookie("ecole");
+    if (ecole_cookie != null) {
+      let ecole = promos.filter(promo => {
+        return promo.api_code === ecole_cookie
+      });
+      ecole_select.value = ecole[0].text;
+      groupe_select.disabled = false;
+
+      on_ecole_selected();
+
+      let groupe_cookie = getCookie("groupe");
+      groupe_select.value = groupe_cookie;
+    }
+
   }
 
 
   function saveGroup() {
-    if (groupe_seleect.value === "") return;
+    if (groupe_select.value === "" || groupe_select.value == "Groupe") return;
     else {
-      setCookie("groupe", groupe_seleect.value, 365);
+      console.log(groupe_select.value)
+      let promo = promos.filter(promo => {
+        return promo.text === ecole_select.value
+      });
+      setCookie("ecole", promo[0].api_code, 365);
+      setCookie("groupe", groupe_select.value, 365);
       document.location.href = "/";
     }
   }
@@ -63,6 +98,35 @@ $this->layout('_template');
       if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
+  }
+
+  function on_ecole_selected() {
+    let selected = ecole_select.value;
+    let promo = promos.filter(promo => {
+      return promo.text === selected
+    });
+    let groupes = promo[0].groupes;
+    clear_groupe_select();
+
+    for (let groupe in groupes) {
+      let option = document.createElement("option");
+      option.innerText = groupes[groupe];
+      groupe_select.append(option);
+      groupe_select.disabled = false;
+    }
+  }
+
+  function clear_groupe_select() {
+    groupe_select.disabled = true;
+    for (var i = 0; i < groupe_select.length; i++) {
+      groupe_select.remove(i);
+    }
+
+    let option = document.createElement("option");
+    option.disabled = true;
+    option.selected = true;
+    option.innerText = "Groupe";
+    groupe_select.append(option)
   }
 
   function eraseCookie(name) {
--- templates/user/login.php
+++ templates/user/login.php
@@ -3,19 +3,28 @@
 $this->layout('_template', ['page_title' => "Se connecter"]);
 ?>
 
-<div class="toast">
-    Cette page est nécessaire seulement pour les étudiants ayant la permission de gerer les devoirs et informations importantes.
+<div class="card p-centered" style="max-width: 400px;">
+    <div class="card-header">
+        <div class="card-title h5">Se connecter</div>
+        <div class="card-subtitle text-gray">
+            <div class="toast bg-warning" style="border: none;">
+                Cette page est nécessaire seulement pour les étudiants ayant la permission de gerer les devoirs et informations importantes.
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <form method="post" class="form-group">
+            <label class="form-label">
+                Username
+                <input class="form-input" name="username" type="text" required>
+            </label>
+
+            <label class="form-label">
+                Password
+                <input class="form-input" name="password" type="password" required>
+            </label>
+
+            <input type="submit" class="btn" value="Login">
+        </form>
+    </div>
 </div>
-<form method="post" class="form-group">
-    <label class="form-label">
-        Username
-        <input class="form-input" name="username" type="text" required>
-    </label>
-
-    <label class="form-label">
-        Password
-        <input class="form-input" name="password" type="password" required>
-    </label>
-
-    <input type="submit" class="btn" value="Login">
-</form>
