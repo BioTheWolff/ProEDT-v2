806addf3321065e9730776ec7eb5a792e585cb79
Rtinox <nathanrouche30@gmail.com>
2021-09-04 18:33:39 +0000

    updated news

--- templates/index.php
+++ templates/index.php
@@ -7,17 +7,20 @@ $this->layout('_template');
 
     <div class="column col-4 col-sm-12 col-md-12">
       <div class="card">
-        <div class="card-image">
-          <img src="/assets/img/logo.png" class="img-responsive" width="100">
-        </div>
         <div class="card-header">
-          <div class="card-title h5">ProEDT</div>
-          <div class="card-subtitle">Auteur: <span class="chip bg-primary">Admin</span></div>
+          <div class="card-title h5">Plage</div>
+          <div class="card-subtitle">Auteur: <span class="chip bg-primary">Emerick [TRES]</span></div>
         </div>
         <div class="card-body">
-          ProEDT est enfin disponible dans sa nouvelle version !<br>
-          N'hésitez pas à nous <a href="/about">contacter</a> si vous avez des idées, des questions ou un soucis.<br>
-          Vous verrez ici prochainement les informations à propos de l'IUT ou encore des informations diffusé par le BDE.
+          Bonjour à tous,<br>
+          Voici le <a href="https://www.covoitribu.fr/poll/19444_ECbiRODJuU9OHb">lien</a> pour le covoiturage de demain pour la plage.<br>
+          Le plus intéressant serait d'avoir un maximum de voitures qui partent de l'IUT.<br>
+          Si vous habitez trop loin essayez de trouver un point de rdv pas loin de chez vous.<br>
+          Il serait aussi bien de récupérer un maximum de personnes qui arriveraient au terminus de la ligne 3.<br>
+          <br>
+          On vous attend nombreux !<br>
+          A dimanche pour les A2.<br>
+          Et on se voit tout à l'heure pour les A1.<br>
         </div>
       </div>
     </div>
@@ -45,5 +48,23 @@ $this->layout('_template');
         </div>
       </div>
     </div>
+
+    <div class="column col-4 col-sm-12 col-md-12">
+      <div class="card">
+        <div class="card-image">
+          <img src="/assets/img/logo.png" class="img-responsive" width="100">
+        </div>
+        <div class="card-header">
+          <div class="card-title h5">ProEDT</div>
+          <div class="card-subtitle">Auteur: <span class="chip bg-primary">Admin</span></div>
+        </div>
+        <div class="card-body">
+          ProEDT est enfin disponible dans sa nouvelle version !<br>
+          N'hésitez pas à nous <a href="/about">contacter</a> si vous avez des idées, des questions ou un soucis.<br>
+          Vous verrez ici prochainement les informations à propos de l'IUT ou encore des informations diffusé par le BDE.
+        </div>
+      </div>
+    </div>
+
   </div>
 </div>
