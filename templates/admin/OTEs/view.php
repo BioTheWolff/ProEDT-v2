1216b6ba6c91a15818e4c5bb7a5a044beda12606
Rtinox <nathanrouche30@gmail.com>
2021-09-12 11:13:00 +0000

    début de la page OTEs viewall

--- templates/admin/OTEs/view.php
+++ templates/admin/OTEs/view.php
@@ -1,1 +1,36 @@
 <?php
+$this->layout('_template');
+?>
+
+<div class="container">
+  <div class="columns">
+    <div class="column col-auto">
+
+      <div class="card p-centered" style="max-width: 400px;">
+        <div class="card-header">
+          <div class="card-title h5">Ajouter</div>
+        </div>
+        <div class="card-body">
+          <form>
+            <input class="form-input" type="text" id="input-example-1" placeholder="Nom/Titre">
+            <input class="form-input" type="text" id="input-example-1" placeholder="Endroit/Localisation">
+            <input class="form-input" type="date" id="input-example-1" placeholder="Date début">
+            <input class="form-input" type="time" id="input-example-1" placeholder="Heure début">
+            <input class="form-input" type="date" id="input-example-1" placeholder="Date fin">
+            <input class="form-input" type="time" id="input-example-1" placeholder="Heure fin">
+
+            <div class="form-group">
+              <label class="form-switch">
+                <input type="checkbox">
+                <i class="form-icon"></i> Journée entiere ?
+              </label>
+            </div>
+
+            <button class="btn">Valider</button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
